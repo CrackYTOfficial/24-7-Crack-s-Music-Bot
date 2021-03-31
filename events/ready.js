4dbedd999cffd5a422ba1bcb19b73a935b7dc188
CrackYTOfficial <81684207+CrackYTOfficial@users.noreply.github.com>
2021-03-31 15:47:30 +0530

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,7 @@
+module.exports = async (client) => {
+  console.log(`[API] Logged in as ${client.user.username}`);
+  await client.user.setActivity("CrackYTOfficial on youtube",{ 
+    type: "WATCHING",
+    url: "https://www.youtube.com/channel/UCJnUlDLXuH24_V9VkOXxEJQ"
+    });
+};
